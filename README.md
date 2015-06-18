47c23d7274c702f9dd8a2d66e6f816b40e2a4075
Jeremy Schaub <schaubjeremy@gmail.com>
2015-06-18 16:11:52 -0500

    Added ability to analyze existing file

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # viz_dstat
 Visualize dstat results on a web page.  Can optionally launch dstat and monitor results in real time.
+
+Uses
+- [dstat](http://dag.wiee.rs/home-made/dstat/)
+- python SimpleHTTPServer for web server
+- [Dygraphs](http://dygraphs.com/) for javascript charts
+
+
--- viz_dstat.sh
+++ viz_dstat.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
+# Launch webserver and visualize dstat measurement in a browser
+# Create dstat file using this format
+#  $ dstat --time -v --net --output [DSTAT_CSV] [DELAY_SEC]
+#  Example:
+#  $ dstat --time -v --net --output example.csv 1 10
+#  $ $0 example.csv
+# If called with no arguments, will launch dstat measurement
 
-usage() {
-  echo Create dstat file using this format
-	echo "   dstat --time -v --net --output [DSTAT_CSV] [DELAY_SEC]"
-	echo Example:
-	echo "   dstat --time -v --net --output example.csv 1 10"
-	echo "   $0 example.csv"
-}
-
-kill_procs() {
-    echo "Stopping dstat"
-    kill $DSTAT_PID > /dev/null
-}
-trap 'kill_procs' SIGTERM SIGINT # Kill process monitors if killed early
 
 #[ $1 -lt 1 ] && usage && exit 1
 
@@ -20,13 +14,29 @@ CSV_FN=$1
 HTML_DIR=html.$(date +"%Y%m%d-%H%M%S")
 mkdir -p $HTML_DIR
 cp -r js $HTML_DIR/.
-cp index.html $HTML_DIR/.
 echo Working directory is $HTML_DIR
+if [ $# -lt 1 ]
+then
+	# Launch dstat measurement
+	kill_dstat() {
+		echo "Stopping dstat"
+		kill $DSTAT_PID > /dev/null
+	}
+	trap 'kill_dstat' SIGTERM SIGINT # Kill dstat when webserver is killed
+	cp index.html $HTML_DIR/.
+	echo Starting dstat using this command:
+	echo "dstat --time -v --net --output dstat.csv"
+	dstat --time -v --net --output dstat.csv > /dev/null &
+	DSTAT_PID=$!
+	ARG="and dstat measurement"
+else
+	echo Visualizing file $1
+	cp $1 $HTML_DIR/.
+	cat index.html | sed "s/dstat.csv/$1/g" > $HTML_DIR/index.html
+fi
+	
 cd $HTML_DIR
-echo Starting dstat using this command:
-echo "dstat --time -v --net --output dstat.csv"
-echo --use CTRL-C to stop
-dstat --time -v --net --output dstat.csv > /dev/null &
-DSTAT_PID=$!
-echo Now starting http server--point your browser to localhost:12121
+
+echo Starting server--point your browser to http://localhost:12121
+echo use CTRL-C to stop web server $ARG
 python -m SimpleHTTPServer 12121
